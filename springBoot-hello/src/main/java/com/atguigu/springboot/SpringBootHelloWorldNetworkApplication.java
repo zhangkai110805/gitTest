b24140ec78ef58e77f060403d2c1e5640ed8e720
zhangkai <zhang@qq.com>
2019-10-10 10:38:16 +0800

    第一次提交项目到Git仓库

---
+++ springBoot-hello/src/main/java/com/atguigu/springboot/SpringBootHelloWorldNetworkApplication.java
@@ -0,0 +1,15 @@
+package com.atguigu.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHelloWorldNetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHelloWorldNetworkApplication.class, args);
+	}
+	public void HelloController() {
+		
+	}
+}
